b9f5c77c118638efd07e77acc33c194f2a48719e
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-02-06 09:47:42 +0200

    Initial release

---
+++ papildvaicajumi.sql
@@ -0,0 +1,47 @@
+/*
+Ko darīt?
+1. Dodies uz https://www.programiz.com/sql/online-compiler/
+2. Pārkopē visu šīs datnes saturu uz Programiz SQL kompilatoru.
+3. Izpildi tālāk dotos uzdevumus.
+4. Kad viss pabeigts, pārkopē rezultātu SQL datnē un skolo.lv iesniedz SQL datni!
+*/
+
+/* Apsveicu! Esi pieņemts darbā kā datu bāzes pārvaldnieks!
+   Tavs pienākums ir strādāt ciešā kontaktā ar biznesa analītiķi
+   Džeinu. Viņa tev ir lūgusi sagatavot vairākus SQL vaicājumus,
+   kuri sniedz datu kopsavilkumu.
+*/
+
+/* 1. Izveido, lūdzu, SQL vaicājumu, kurš izvada kopsavilkumu par klientu
+      vecumu un valstīm no tabulas customers. Atgriezto kolonnu nosaukumi
+      doti zemāk:
+       ---------------------------------------------------------------------------
+      | Customer count | Min age | Max age | Average age | Distinct countries |
+       ---------------------------------------------------------------------------
+ */
+ 
+ 
+ 
+ /* 2. Izveido, lūdzu, SQL vaicājumu, kurš izvada, cik daudz pasūtījumu ir katram
+       klientam un cik daudz preču klients ir pasūtījis, saskaitot preces no visiem
+       pasūtījumiem. Atgriezto kolonnu nosaukumi doti zemāk:
+       ----------------------------------------------
+      | Customer ID | Order count | Total item count |
+       ----------------------------------------------
+ */
+ 
+ 
+ 
+ /* 3. Izveido, lūdzu, SQL vaicājumu, kurš atgriež tikai to klientu
+    vārdus, uzvārdus, kuru piegādes šobrīd vēl nav izpildītas
+    (status ir "Pending").
+ */
+
+
+
+  /* Kādus vēl interesantus datus, kopsavilkumu par tiem tu vari
+     sniegt Džeinai? Izdomā pats!
+ */
+ 
+ 
+ 
