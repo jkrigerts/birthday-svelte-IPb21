2dfc88e58586c16ef3bacc835843a6890d80f383
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-02-07 14:59:51 +0200

    01. For Leons

---
+++ src/routes/birthdays/Birthday.test.js
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+
+import Birthday from './Birthday.svelte';
+
+describe('Birthday', () => {
+	it('has the name of a person', () => {
+		render(Birthday, { name: 'Hercules' });
+		expect(screen.queryByText);
+	});
+});
